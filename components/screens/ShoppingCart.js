c6eaf981ec8a4f5641a860bda1fee44619f3d48a
aweiler97 <aweiler@unca.edu>
2021-03-10 15:35:37 -0500

    Cart funcionality and formatting

--- components/screens/ShoppingCart.js
+++ components/screens/ShoppingCart.js
@@ -1,16 +1,34 @@
 import React, {useState} from 'react';
-import { Button } from 'react-native';
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { Alert, Button } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, Image } from 'react-native';
 import NavBar from '../NavBar';
 
 function ShoppingCart({ screenProps,navigation }) {
 
     const [shoppingCart, setShoppingCart] = useState(screenProps.shoppingCart); 
 
+    let totalPrice = 0;
+
+    const createList = () => {
+        return shoppingCart.map((element, index) => {
+            totalPrice += element.price;
+            return (
+                <View style={styles.product_container} key={index}>
+                    <Image style={styles.product_image} source={element.image} />
+                    <Text style={styles.product_name}>{element.name}</Text>
+                    <Text style={styles.product_price}>{element.price}</Text>
+                    <Button title='remove' style={styles.remove_button} onPress={()=>removeShoppingCartItem(element.key)}></Button>
+                </View>
+            );
+        });
+    };
+
+    const [list, setList] = useState(createList());
+
     const removeShoppingCartItem = (itemKey) =>{
         alert('Item Removed');
 
-        let updatedList = screenProps.shoppingCart
+        let updatedList = screenProps.shoppingCart;
         for(let index = 0; index < updatedList.length; index++){
             if(updatedList[index].key == itemKey){
                 updatedList.splice(index,1);
@@ -19,28 +37,27 @@ function ShoppingCart({ screenProps,navigation }) {
 
         setShoppingCart(updatedList);
         screenProps.shoppingCartButtonPress(updatedList);
+        setList(createList);
     }
 
-    let totalPrice = 0;
+    const clearList = () => {
+        Alert.alert('Items purchased!');
+        let emptyList = screenProps.shoppingCart.splice();
 
-    const list = () => {
-        return shoppingCart.map((element) => {
-          totalPrice += element.price;
-            return (
-                <View style={styles.product_container}>
-                    <Text style={styles.product_name}>{element.name}</Text>
-                    <Text style={styles.product_price}>{element.price}</Text>
-                    <Button title='remove' style={styles.remove_button} onPress={()=>removeShoppingCartItem(element.key)}></Button>
-                </View>
-            );
-        });
-    };
+        setShoppingCart(emptyList);
+        screenProps.shoppingCartButtonPress(emptyList);
+        setList(createList);
+    }
+
     return (
         <View style={styles.container}>
             <ScrollView>
-                <View style={styles.cart_container}>
-                    <View>{list()}</View>
+                <View>
+                    <View>{list}</View>
                     <Text style={styles.total_price}>Total: ${totalPrice}</Text>
+                    <Button title='Purchase' style={styles.purchase_button} onPress={()=> {
+                        clearList();
+                    }}></Button>
                 </View>
             </ScrollView>
             <NavBar navigation={navigation}></NavBar>
@@ -49,34 +66,42 @@ function ShoppingCart({ screenProps,navigation }) {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    container: {        
         flex: 1,
         paddingTop: 40,
     },
     cart_container: {
-      alignItems: 'center',
+        alignItems: 'center',
     },
     product_container: {
-      flex: 1,
-      margin: 10,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+    },
+    product_image: {
+        height: 70,
+        width: 70,
     },
     product_name: {
-      paddingEnd: 10,
+        fontSize: 20,
     },
     product_price: {
-      
+        fontWeight: 'bold',
+        fontSize: 15,
     },
     remove_button: {
 
     },
     total_price: {
-
+        paddingTop: 50,
+        textAlign: 'center', 
+        alignSelf: 'stretch',
+        fontWeight: 'bold',
+        fontSize: 23,
     },
     purchase_button: {
-
+        
     }
   });
 
-export default ShoppingCart
+export default ShoppingCart;
