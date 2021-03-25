5882000823ed22c16ce97761a97be560a8cafd1d
Puett <andrew.puett@capgemini.com>
2021-03-25 10:05:40 -0500

    Procfile

--- backend/routes.js
+++ backend/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Product = require('./ProductModels')
 
-router.post('/pp',(request,response)=>{
+router.post('/post-product',(request,response)=>{
     const addedProduct = new Product({
         key: request.body.key,
         name: request.body.name,
