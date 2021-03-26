1988d90ff790010f476408897b70ee5914bd2578
Puett <andrew.puett@capgemini.com>
2021-03-26 12:49:40 -0500

    added Products

--- components/ProductContext.js
+++ components/ProductContext.js
@@ -1,6 +1,8 @@
 const images = {
   laptop: require('./images/laptop.png'),
-  charger: require('./images/charger.png')
+  charger: require('./images/charger.png'),
+  usb_station: require('./images/usb-station.png'),
+  raspberry_pi: require('./images/raspberry-pi.png')
 }
 
 export default images;
